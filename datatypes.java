c0d6106c43d42576a5ed217396f564ef60c84ef2
sv742744 <shashi09op@gmail.com>
2024-12-02 14:42:42 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,13 @@
+class datatypes{
+    public static void main (String args []){
+        int a=10;
+        float f=90.8f;
+        char c= 'b';
+        double d=70.99;
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+
+    }
+}

7594a876c58e5d8d8f642e00803f97829fa5d5fe
sv742744 <shashi09op@gmail.com>
2024-12-02 15:03:14 +0530

    Add files via upload

---
+++ asciiconversion.java
@@ -0,0 +1,9 @@
+class asciiconversion{
+    public static void main (String args []){
+        int a=55;
+        char c=(char)(a);
+        char b='7';
+        System.out.println(c);
+        System.out.println(b);
+    }
+}

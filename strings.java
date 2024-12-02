3e0f52db3dc6fcd063c4e9c12ffec3037458c953
sv742744 <shashi09op@gmail.com>
2024-12-02 16:02:18 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class strings {
+    public static void main (String args []){
+        Scanner sc =new Scanner (System.in);
+        String a=sc.next();
+        System.out.println("congratulatoin "+a+"   you won 5 lakhs");
+        System.out.printf("congratulation %s  you won 5 lakhs",a);
+    
+
+
+    }
+}

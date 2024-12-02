3e17bada0bae5cb264a3ccfe9ddc2f140e3f0e7a
sv742744 <shashi09op@gmail.com>
2024-12-02 15:17:06 +0530

    Add files via upload

---
+++ printnextchar.java
@@ -0,0 +1,10 @@
+class printnextchar{
+    public static void main (String args[]){
+        char a='s';
+        int b=a;
+        b+=1;
+        char c=(char)b;
+        System.out.println(c);
+
+    }
+}
